9f1544ac558066f25ac60d0c12245e93a5ebc287
Filipp <filippstryukop@gmail.com>
2019-08-25 21:22:14 +0300

    Update jscode.js

--- jscode.js
+++ jscode.js
@@ -1,15 +1,13 @@
-"use strict"
-
-const mainBlock = document.getElementsByClassName("main-square")[0];
-const square = document.getElementsByClassName("square");
+const mainBlock = document.getElementsByClassName("main-squar")[0];
+const squar = document.getElementsByClassName("squar");
 let move = "first";
-let squareArray = [];
+let squarArray = [];
 
 for(let i = 0; i < 9; i++){
-    squareArray.push(square[i]);
+    squarArray.push(squar[i]);
 }
 
-squareArray.forEach(x => {
+squarArray.forEach(x => {
     addColorToBlock(x);
 });
 
@@ -18,44 +16,21 @@ function winAndReload(color) {
 
     //Main block style
 
-    if(color === "yellow") {
+    if(color == "yellow") {
         mainBlock.style = "animation: shadowYellow 2s linear";
-    }else if(color === "orange") {
+    }else if(color == "orange") {
         mainBlock.style = "animation: shadowOrange 2s linear";
-    }else if(color === "draw") {
+    }else if(color == "draw") {
         mainBlock.style = "animation: shadowDraw 2s linear";
     }
     for(let i = 0; i < 9; i++) {
-        square[i].style = "display: none";
+        squar[i].style = "display: none";
     }
     setTimeout(() => {
         location.reload();
     }, 1500);
     
 }
-
-function algorithm(x){
-    
-    let mainArray = [];
-    let xCoordinates = [0];
-    let arr = [];   
-
-    for(let i = 0; i < x - 1; i++) {
-        xCoordinates.push(xCoordinates[i] + x);
-    }
-
-    let alterArray = [];
-    alterArray.push(xCoordinates);
-    xCoordinates.forEach(function(currentValue){
-        
-        arr[0] = (currentValue + 1);
-        arr[1] = (currentValue + 2);
-
-    });
-    alterArray.push(arr);
-    console.log(alterArray);
-}
-algorithm(3);
 
 let arrayCombinations = [
     [0, 1, 2],
@@ -66,37 +41,37 @@ let arrayCombinations = [
     [2, 4, 6],
     [0, 3, 6],
     [2, 5, 8]    
-];
+]
 
-addEventListener("click", () => {
+function classContains(obj, name) {
+    return obj.classList.contains(name);
+}
+
+addEventListener("click", function(){
     let coo1, coo2, coo3;
-    arrayCombinations.forEach(function(currentValue) {
+    arrayCombinations.forEach(function(currentValue, index) {
         [coo1, coo2, coo3] = currentValue;
-        if(classContains(square[coo1], "first") && classContains(square[coo2], "first") && classContains(square[coo3], "first")) {
+        if(classContains(squar[coo1], "first") && classContains(squar[coo2], "first") && classContains(squar[coo3], "first")) {
             winAndReload("yellow");
         }
-        if(classContains(square[coo1], "second") && classContains(square[coo2], "second") && classContains(square[coo3], "second")) {
+        if(classContains(squar[coo1], "second") && classContains(squar[coo2], "second") && classContains(squar[coo3], "second")) {
             winAndReload("orange");
         }    
     });
 });
 
 
-function addColorToBlock(arg) {
-    arg.addEventListener("click", () => {
-        if(move === "first" && !arg.classList.contains("second")) {
+function addColorToBlock(ARG) {
+    ARG.addEventListener("click", function(){
+        if(move == "first" && !ARG.classList.contains("second")) {
             move = "second";
-            arg.style = "background-color: yellow";
-            arg.classList.add("first");
-        }else if(move === "second" && !arg.classList.contains("first")) {
+            ARG.style = "background-color: yellow";
+            ARG.classList.add("first");
+        }else if(move == "second" && !ARG.classList.contains("first")) {
             move = "first";
-            arg.style = "background-color: orange";
-            arg.classList.add("second");
+            ARG.style = "background-color: orange";
+            ARG.classList.add("second");
         }
         
     });     
-}
-
-function classContains(obj, name) {
-    return obj.classList.contains(name);
 }
